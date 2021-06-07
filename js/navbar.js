3c21bbf57146ab57ff079499c4dc4414208a3bb9
JColin-dev <colin_dev@outlook.fr>
2021-06-07 11:16:38 +0200

    nav

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+const navBar = [
+    {
+        nom: "home",
+        lien: '#home'
+    },
+    {
+        nom: "our work",
+        lien: '#work'
+    },
+    {
+        nom: "pricing",
+        lien: '#price'
+    },
+    {
+        nom: "contact",
+        lien: '#contact'
+    },
+]
+
+let barreNav = document.querySelector("#NavJs");
+
+for(let i in navBar) {
+let monElem = document.createElement("a")
+monElem.innerHTML = navBar[i].nom
+monElem.href = navBar[i].lien
+barreNav.appendChild(monElem);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
